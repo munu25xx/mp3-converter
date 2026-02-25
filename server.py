bfffb19fd2dd2a313a759e20bfd2a3c7591bd426
munu25xx <zinroukeisisei+x@gmail.com>
2026-02-25 20:36:25 +0900

    Update server.py

--- server.py
+++ server.py
@@ -8,8 +8,11 @@ def upload():
     if request.method == "POST":
         f = request.files["file"]
         f.save("input")
-        subprocess.run(["ffmpeg","-i","input","output.mp3"])
+
+        subprocess.run(["ffmpeg", "-i", "input", "output.mp3"])
+
         return send_file("output.mp3", as_attachment=True)
+
     return '''
     <form method="post" enctype="multipart/form-data">
     <input type="file" name="file">
